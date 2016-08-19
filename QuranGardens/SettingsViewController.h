0c6e8e61b15e3e6028e62bbb7e5769888f67fbce
Amr Lotfy <amr.lotfy@badrit.com>
2016-08-19 10:26:13 +0200

    Add SettingsViewController
    Removed unused image from cell

---
+++ QuranGardens/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  QuranGardens
+//
+//  Created by Amr Lotfy on 8/19/16.
+//  Copyright © 2016 Amr Lotfy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
