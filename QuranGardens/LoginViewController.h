c0d7dc356df69e961663dd573e6fa82230e295d0
Amr Lotfy <amr@motazs-mac-mini.local>
2017-01-07 17:50:41 +0200

    Creating login screens

---
+++ QuranGardens/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  QuranGardens
+//
+//  Created by Amr Lotfy on 1/7/17.
+//  Copyright © 2017 Amr Lotfy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ QuranGardens/LoginViewController.m
@@ -0,0 +1,43 @@
+//
+//  LoginViewController.m
+//  QuranGardens
+//
+//  Created by Amr Lotfy on 1/7/17.
+//  Copyright © 2017 Amr Lotfy. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@property (strong, nonnull) UITextField *email;
+@property (strong, nonnull) UITextField *password;
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
